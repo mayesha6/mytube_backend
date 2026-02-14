153a0e52d6902d5ec2221ab10b24711d2895ee67
mayesha6 <soumyrhmn234@gmail.com>
2026-02-14 11:15:58 +0600

    subscription

---
+++ src/app/modules/subscription/subscription.interface.ts
@@ -0,0 +1,13 @@
+export enum Package {
+  FREE = "FREE",
+  GOLD = "GOLD",    
+  DIAMOND = "DIAMOND",
+  PLATINUM = "PLATINUM"
+}
+
+export enum SubscriptionStatus {
+  ACTIVE = "ACTIVE",
+  INCOMPLETE = "INCOMPLETE",
+  CANCELLED = "CANCELLED",
+  PAST_DUE = "PAST_DUE"
+}
